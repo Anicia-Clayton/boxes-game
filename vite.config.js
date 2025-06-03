b56b57edd9e8acb5816a351fa47281f49a3d48a8
Anicia-Clayton <108688192+Anicia-Clayton@users.noreply.github.com>
2025-06-02 19:38:30 -0500

    Updated Vite config & package dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.js',
+      },
+      output: {
+        entryFileNames: '[name].pack.js',
+      }
+    }
+  }
+});
